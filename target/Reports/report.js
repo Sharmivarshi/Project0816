8ea74b05b5ccf278084cb2c63b57fd1067b4101c
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 <sharmisharmi39@gmail.com>
2022-08-18 19:50:02 +0530

    added

--- target/Reports/report.js
+++ target/Reports/report.js
@@ -1,93 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/featureFiles/ForgotPassword.feature");
-formatter.feature({
-  "name": "Validate the forgot password functionality",
-  "description": "",
-  "keyword": "Feature",
-  "tags": [
-    {
-      "name": "@smoke"
-    }
-  ]
-});
-formatter.scenario({
-  "name": "Validate the forgot Password with invalid email id",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@smoke"
-    },
-    {
-      "name": "@testingforgot"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should launch chrome and load the Url",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "LoginStepDefinition.user_should_launch_chrome_and_load_the_Url()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should click the forgot password link",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "ForgotStepDefinition.user_should_click_the_forgot_password_link()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should input ivalid email id",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ForgotStepDefinition.user_should_input_ivalid_email_id()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should click the Send button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "ForgotStepDefinition.user_should_click_the_Send_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should navigate to Home page",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "ForgotStepDefinition.user_should_navigate_to_Home_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("file:src/test/resources/featureFiles/Login.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/featureFiles/Login.feature");
 formatter.feature({
   "name": "To validate the login functionality",
   "description": "",
@@ -120,7 +31,7 @@ formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Validate Login funtionality with valid username and invalid Password",
+  "name": "Validate Login funtionality with invalid username and invalid Password",
   "description": "",
   "keyword": "Scenario",
   "tags": [
@@ -128,28 +39,53 @@ formatter.scenario({
       "name": "@sanity"
     },
     {
-      "name": "@testing"
+      "name": "@test"
     }
   ]
 });
 formatter.step({
-  "name": "User should pass valid username and invalid Password",
+  "name": "User should pass invalid username and invalid Password",
   "rows": [
     {
       "cells": [
-        "sharm@gmail.com",
-        "Sharmi@123",
-        "jshi@gmail.com",
-        "Jershi@377",
-        "mani",
-        "Mani@334"
+        "language",
+        "mobile",
+        "laptop"
+      ]
+    },
+    {
+      "cells": [
+        "java",
+        "vivo",
+        "hp"
+      ]
+    },
+    {
+      "cells": [
+        "selenium",
+        "lg",
+        "acer"
+      ]
+    },
+    {
+      "cells": [
+        "sql",
+        "nokia",
+        "dell"
+      ]
+    },
+    {
+      "cells": [
+        "oracle",
+        "Mi",
+        "lenovo"
       ]
     }
   ],
   "keyword": "When "
 });
 formatter.match({
-  "location": "LoginStepDefinition.user_should_pass_valid_username_and_invalid_Password(DataTable)"
+  "location": "LoginStepDefinition.user_should_pass_invalid_username_and_invalid_Password(DataTable)"
 });
 formatter.result({
   "status": "passed"
@@ -183,4 +119,218 @@ formatter.after({
 formatter.after({
   "status": "passed"
 });
+formatter.scenarioOutline({
+  "name": "Validate Login funtionality with invalid username and valid Password",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "User should pass invalid username\"\u003cemail\u003e\"  and valid Password\"\u003cpassword\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "User should click the login button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "user should navigate to incorrect credential page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "email",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "sharmi",
+        "Sharmi@123"
+      ]
+    },
+    {
+      "cells": [
+        "saru@gmail.com",
+        "Saru@i23"
+      ]
+    },
+    {
+      "cells": [
+        "vijay@gmail.com",
+        "Vijay@123"
+      ]
+    },
+    {
+      "cells": [
+        "jershi@gmail.com",
+        "Jershi@123"
+      ]
+    }
+  ]
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should launch chrome and load the Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_launch_chrome_and_load_the_Url()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate Login funtionality with invalid username and valid Password",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@sanity"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "User should pass invalid username\"sharmi\"  and valid Password\"Sharmi@123\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_pass_invalid_username_and_valid_Password(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should click the login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_click_the_login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should navigate to incorrect credential page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_navigate_to_incorrect_credential_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User should launch chrome and load the Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_launch_chrome_and_load_the_Url()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate Login funtionality with invalid username and valid Password",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@sanity"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "User should pass invalid username\"saru@gmail.com\"  and valid Password\"Saru@i23\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_pass_invalid_username_and_valid_Password(String,String)"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"(//input[@type\u003d\u0027email\u0027])[1]\"}\n  (Session info: chrome\u003d104.0.5112.81)\nFor documentation on this error, please visit: https://selenium.dev/exceptions/#no_such_element\nBuild info: version: \u00274.1.4\u0027, revision: \u0027535d840ee2\u0027\nSystem info: host: \u0027USER-PC\u0027, ip: \u0027192.168.1.4\u0027, os.name: \u0027Windows 7\u0027, os.arch: \u0027amd64\u0027, os.version: \u00276.1\u0027, java.version: \u00271.8.0_202\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [515b7382d4fca91d6d87b8456970c8c1, findElement {using\u003dxpath, value\u003d(//input[@type\u003d\u0027email\u0027])[1]}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 104.0.5112.81, chrome: {chromedriverVersion: 104.0.5112.79 (3cf3e8c8a07d..., userDataDir: C:\\Users\\SHARMI~1.USE\\AppDa...}, goog:chromeOptions: {debuggerAddress: localhost:51865}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:51865/devtoo..., se:cdpVersion: 104.0.5112.81, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 515b7382d4fca91d6d87b8456970c8c1\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:184)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:167)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:142)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:567)\r\n\tat org.openqa.selenium.remote.ElementLocation$ElementFinder$2.findElement(ElementLocation.java:162)\r\n\tat org.openqa.selenium.remote.ElementLocation.findElement(ElementLocation.java:60)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:385)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:377)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:70)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:39)\r\n\tat com.sun.proxy.$Proxy22.sendKeys(Unknown Source)\r\n\tat org.utilities.BaseClass.fill(BaseClass.java:88)\r\n\tat org.stepdefinition.LoginStepDefinition.user_should_pass_invalid_username_and_valid_Password(LoginStepDefinition.java:95)\r\n\tat ✽.User should pass invalid username\"saru@gmail.com\"  and valid Password\"Saru@i23\"(file:src/test/resources/featureFiles/Login.feature:27)\r\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "User should click the login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_click_the_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "user should navigate to incorrect credential page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_navigate_to_incorrect_credential_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.embedding("image/png", "embedded0.png", null);
+formatter.after({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
 });
